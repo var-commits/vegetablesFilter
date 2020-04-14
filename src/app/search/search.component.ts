c9c5373a39424abcab2c206fa7c7a917017eaaed
Siddharth <siddharth@Apples-MacBook-Pro.local>
2020-04-14 12:00:22 +0530

    Vegetables filter

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { DataService } from "../data.service";
+@Component({
+  selector: "search-component",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.css"],
+})
+export class SearchComponent implements OnInit {
+  searchForm: FormGroup;
+  constructor(private fb: FormBuilder, public service: DataService) {}
+
+  ngOnInit() {
+    // add corresponding validators
+    this.searchForm = this.fb.group({
+      searchVegetable: ["", [Validators.required, Validators.minLength(1)]],
+    });
+    this.service.currentVegetableName.subscribe();
+    // write a function that calls changeVegetableName upon value change in the form
+  }
+  getVegetableNames(name) {
+    this.searchForm.controls["searchVegetable"].setValue(
+      name.currentTarget.value
+    );
+    this.service.changeVegetableName(name.currentTarget.value);
+  }
+}
